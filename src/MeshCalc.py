f284b88e5981f7bd3be40a4b8602f65726124f7e
/~tersite1 <160453556+tersite1@users.noreply.github.com>
2024-05-27 23:40:13 +0900

    Update MeshCalc.py

--- src/MeshCalc.py
+++ src/MeshCalc.py
@@ -2,13 +2,13 @@ import bpy
 import bmesh
 
 def Meshcalc(file_path):
-    # OBJ 파일을 임포트합니다.
+   
     bpy.ops.import_scene.obj(filepath=file_path)
 
-    # 활성 오브젝트를 가져옵니다.
+    # 
     obj = bpy.context.selected_objects[0]
 
-    # bmesh를 사용하여 메쉬 데이터를 계산합니다.
+    # mesh data calc
     bm = bmesh.new()
     bm.from_mesh(obj.data)
     bm.transform(obj.matrix_world)
@@ -24,24 +24,23 @@ def Meshcalc(file_path):
             if v.co[i] > max_coord[i]:
                 max_coord[i] = v.co[i]
 
-    bm.free()  # bmesh 클리어
+    bm.free()  # bmesh clear
 
-    # 큐브의 중심과 크기 계산
+    # cube calc
     center = [(min_coord[i] + max_coord[i]) / 2 for i in range(3)]
     size = [(max_coord[i] - min_coord[i]) for i in range(3)]
 
-    # 큐브 생성
+    # cube gen
     bpy.ops.mesh.primitive_cube_add(size=1, location=center)
     cube = bpy.context.object
     cube.scale = [size[0]/2, size[1]/2, size[2]/2]
 
-    # 투명한 재질 설정
+    # material
     material = bpy.data.materials.new(name="TransparentMaterial")
     material.use_nodes = True
     material.node_tree.nodes["Principled BSDF"].inputs["Alpha"].default_value = 0.3
     material.blend_method = 'BLEND'
     cube.data.materials.append(material)
 
-# 이제 Meshcalc 함수를 사용하여 작업을 수행할 수 있습니다.
-file_path = '/Users/caz/Downloads/3DModel/OBJ/3DModel.obj'
+file_path = 'your_path.obj'
 Meshcalc(file_path)
